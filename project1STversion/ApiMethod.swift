5e3d613f50c2d706549904bcaae56ab00bc831ff
EhsanHassan1997 <49812159+EhsanHassan1997@users.noreply.github.com>
2019-04-26 14:57:45 +0200

    omar2

---
+++ project1STversion/ApiMethod.swift
@@ -0,0 +1,17 @@
+//
+//  ApiMethod.swift
+//  Forecast
+//
+//  Created by Apple on 3/1/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+class ApiMethod{
+    class func forecastData(compilation:()->Void){
+        
+    }
+}
