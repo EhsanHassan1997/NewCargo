de47c4ba1a45706e979a94fbbebfb2889025cff0
EhsanHassan1997 <ehsanhassan2015@gmail.com>
2019-06-06 13:12:54 +0200

    tany yom el 3id 1

---
+++ project1STversion/Model/RequestDetaile.swift
@@ -0,0 +1,22 @@
+//
+//  RequestDetaile.swift
+//  project1STversion
+//
+//  Created by Ehsan Hassan on 6/5/19.
+//  Copyright © 2019 Omar Kelany. All rights reserved.
+//
+
+import Foundation
+class RequestDetaile{
+    var cargoType : String?
+    var Quantity : Int?
+    var Weight : Double?
+    var Volume : Double?
+    var Width : Double?
+    var Hieght: Double?
+    var Length: Double?
+    var PickUp : String?
+    var Destination : String?
+    var StartDate : Date?
+    var EndDate : Date?
+}
--- project1STversion/View/ShowRequestViewController.swift
+++ project1STversion/View/ShowRequestViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import LGButton
 
 class ShowRequestViewController: UIViewController {
-
+    
     @IBOutlet weak var ImportExportLogoImage: UIImageView!
     @IBOutlet weak var ImportExportNameLable: UILabel!
     @IBOutlet weak var CargoType: UILabel!
@@ -28,12 +28,69 @@ class ShowRequestViewController: UIViewController {
     @IBOutlet weak var AcceptConditionButton: UIImageView!
     @IBOutlet weak var IgnoreButton: LGButton!
     @IBOutlet weak var AcceptButton: LGButton!
+    var  ReqTOTO : RequestDetaile!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        //        ReqTOTO.cargoType = "liqued"
+        //        ReqTOTO.Quantity = 3
+        //        ReqTOTO.Weight = 4.5
+        //        ReqTOTO.Hieght = 2
+        //        ReqTOTO.Length = 4
+        //        ReqTOTO.Width = 3
+        
+        //        ReqTOTO.StartDate = Date()
+        //        var SdateComponents = DateComponents()
+        //        SdateComponents.year = 2019
+        //        SdateComponents.month = 7
+        //        SdateComponents.day = 20
+        //
+        //        ReqTOTO.EndDate = Date()
+        //        var EdateComponents = DateComponents()
+        //        EdateComponents.year = 2020
+        //        EdateComponents.month = 2
+        //        EdateComponents.day = 10
+        
+        //        ReqTOTO.Volume = ReqTOTO.Weight! *  ReqTOTO.Hieght! * ReqTOTO.Width!
+        
+        //        ReqTOTO.PickUp = " 23 st Giza Tahrer"
+        //        ReqTOTO.Destination = "34 st cairo naser"
+        
+        //        CargoType.text = ReqTOTO.cargoType
+        //        QuantityOfCargo.text = String(ReqTOTO.Quantity!)
+        //        WeightOfCargo.text = String(ReqTOTO.Weight!)
+        //        VolumeOfCargo.text = String(ReqTOTO.Volume!)
+        //        WidthOfCargo.text = String(ReqTOTO.Width!)
+        //        LenghtOfCargo.text = String(ReqTOTO.Length!)
+        //        PickUpLocation.text = String(ReqTOTO.PickUp!)
+        //        DestinationLocation.text = String(ReqTOTO.Destination!)
+        
+        //       let userCalendar = Calendar.current // user calendar
+        //       let someDateTime = userCalendar.date(from: SdateComponents)
+        
+        //        let formatter = DateFormatter()
+        //        formatter.dateFormat = "yyyy/MM/dd"
+        //        let someDateTime = formatter.date(from: ReqTOTO.StartDate )
+        let userCalendar = Calendar.current
+        //        ReqTOTO.StartDate = Date()
+        var SdateComponents = DateComponents()
+        SdateComponents.year = 2019
+        SdateComponents.month = 7
+        SdateComponents.day = 20
+        //        SdateComponents.dateFormat = "dd/MM/yyyy"
+        let firstLandPhoneCallDate = userCalendar.date(from: SdateComponents)!
+        let myFormatter = DateFormatter()
+        
+        //        myFormatter.string(from: firstLandPhoneCallDate) // What gives?
+        
+        print("jhhgcf",myFormatter.string(from: firstLandPhoneCallDate))
+        StartDate.text = myFormatter.string(from: firstLandPhoneCallDate)
+        //        label.text = result
+        
         ImportExportLogoImage.layer.cornerRadius = ImportExportLogoImage.bounds.width / 2
         ImportExportLogoImage.clipsToBounds = true
-
+        
         // Do any additional setup after loading the view.
     }
     
@@ -47,13 +104,14 @@ class ShowRequestViewController: UIViewController {
         
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
