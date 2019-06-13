1a0de1d27df1b5d605297e1ece90c6f146c00971
EhsanHassan1997 <ehsanhassan2015@gmail.com>
2019-06-13 20:00:47 +0200

    13/6/2019-4

---
+++ project1STversion/View/DriverCurrentLocationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DriverCurrentLocationViewController.swift
+//  project1STversion
+//
+//  Created by Ehsan Hassan on 6/13/19.
+//  Copyright © 2019 Omar Kelany. All rights reserved.
+//
+
+import UIKit
+
+class DriverCurrentLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
