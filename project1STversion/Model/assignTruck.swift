29b48d63f148c835e347ab99070c98c050003146
EhsanHassan1997 <ehsanhassan2015@gmail.com>
2019-06-15 20:20:11 +0200

    ehsanaya 2.2

---
+++ project1STversion/Model/assignTruck.swift
@@ -0,0 +1,26 @@
+//
+//  assignTruck.swift
+//  project1STversion
+//
+//  Created by Ehsan Hassan on 6/15/19.
+//  Copyright © 2019 Omar Kelany. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class assignTruck {
+    //var select : UIButton!
+//    var image : UIImage!
+    var drivername : String!
+    var trucktype : String!
+    var platformnumber : String!
+    
+    init(_ i : UIImage , _ d : String ,  _ t : String , _ p : String) {
+        //self.select = s
+//        self.image = i
+        self.drivername = d
+        self.trucktype = t
+        self.platformnumber = p
+    }
+    
+}
---
+++ project1STversion/View/TruckTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TruckTableViewCell.swift
+//  project1STversion
+//
+//  Created by Ehsan Hassan on 6/15/19.
+//  Copyright © 2019 Omar Kelany. All rights reserved.
+//
+
+import UIKit
+
+class TruckTableViewCell: UITableViewCell {
+    @IBOutlet weak var driverName: UILabel!
+    @IBOutlet weak var truckType: UILabel!
+    @IBOutlet weak var platformNumber: UILabel!
+    
+        func SetTruckTableViewCell(TrucKChoosed : assignTruck){
+             driverName.text = TrucKChoosed.drivername
+             truckType.text = TrucKChoosed.trucktype
+            platformNumber.text = TrucKChoosed.platformnumber
+    }
+}
